d5f146cb29454df71576c105fb3fedea616b01ba
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-04-02 18:03:42 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("multiplatform") version "1.4-M1"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+application {
+    mainClassName = "AppKt"
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
+    jvm {
+        withJava()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+    }
+}
+
+//WA:
+//distributions {
+//    main {
+//        contents {
+//            from("$buildDir/libs") {
+//                rename("${rootProject.name}-jvm", rootProject.name)
+//                into("lib")
+//            }
+//        }
+//    }
+//}
